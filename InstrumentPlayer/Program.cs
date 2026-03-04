208053fd8c2dff856b7ab86259501df526d1a42c
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-04 14:55:57 +0900

    InstrumentPlayer 완료

--- InstrumentPlayer/Program.cs
+++ InstrumentPlayer/Program.cs
@@ -1,4 +1,21 @@
 using System;
+using System.Text;
+using InstrumentPlayer;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.OutputEncoding = Encoding.UTF8;
+
+Instrument[] instruments = new Instrument[] {
+    new Piano(),
+    new Guitar(),
+    new Drum()
+};
+
+Console.WriteLine($"=== 악기 연주회 ===");
+foreach (var instrument in instruments) {
+    instrument.Tune();
+    instrument.Play();
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine();
